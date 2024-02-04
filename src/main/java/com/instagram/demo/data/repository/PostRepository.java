334e3df9a89e720477cef42404f4bcdd9c6f3d18
Zakariae Moutaouakil <moutaouakilzakariae.cpgea@gmail.com>
2024-02-04 17:49:56 +0100

    Save Before changing schema

--- src/main/java/com/instagram/demo/data/repository/PostRepository.java
+++ src/main/java/com/instagram/demo/data/repository/PostRepository.java
@@ -2,7 +2,9 @@ package com.instagram.demo.data.repository;
 
 import com.instagram.demo.data.projection.PostProjection;
 import com.instagram.demo.data.schema.Post;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -10,5 +12,9 @@ public interface PostRepository extends CrudRepository<Post, Long> {
     // Counts number of posts uploaded by a specific user given its username
     long countByUploaderUsername(String username);
 
-    List<PostProjection> findPostsByUploaderUsername(String username);
+//    List<PostProjection> findPostsByUploaderUsername(String username);
+
+//    @Query("SELECT p FROM Post p LEFT JOIN FETCH p.likers LEFT JOIN FETCH p.comments WHERE p.uploader.username = :username")
+    List<PostProjection> findPostsByUploaderUsername(@Param("username") String username);
+    List<Post> findAllByUploaderUsername(String username);
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -22,26 +22,37 @@
 
 
 -- Inserting Persons
-INSERT INTO Person (username, email, password, firstname, lastname, bio,validated,photo) VALUES
-                                                                             ('john_doe', 'john.doe@example.com', 'password123', 'John', 'Doe', 'This is John Doe.',TRUE,'https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg'),
-                                                                             ('jane_smith', 'jane.smith@example.com', 'password456', 'Jane', 'Smith', 'Hello, I am Jane.',TRUE,'https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg'),
-                                                                             ('bob_jackson', 'bob.jackson@example.com', 'password789', 'Bob', 'Jackson', 'Bob here!',FALSE,'https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg');
+INSERT INTO Person (username, email, password, firstname, lastname, bio, validated, photo)
+VALUES ('john_doe', 'john.doe@example.com', 'password123', 'John', 'Doe', 'This is John Doe.', TRUE,
+        'https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg'),
+       ('jane_smith', 'jane.smith@example.com', 'password456', 'Jane', 'Smith', 'Hello, I am Jane.', TRUE,
+        'https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg'),
+       ('bob_jackson', 'bob.jackson@example.com', 'password789', 'Bob', 'Jackson', 'Bob here!', FALSE,
+        'https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg');
 
 -- Inserting Posts
-INSERT INTO Post (description,  image, uploader_id) VALUES
-                                                                 ('First post by John',  'https://img.freepik.com/photos-gratuite/peinture-lac-montagne-montagne-arriere-plan_188544-9126.jpg?size=626&ext=jpg&ga=GA1.1.1788068356.1706832000&semt=sph', 1),
-                                                                 ('Janes travel adventure',  'https://images.ctfassets.net/hrltx12pl8hq/28ECAQiPJZ78hxatLTa7Ts/2f695d869736ae3b0de3e56ceaca3958/free-nature-images.jpg?fit=fill&w=1200&h=630', 2),
-    ('Bobs coding journey', 'https://img.freepik.com/photos-gratuite/peinture-numerique-montagne-arbre-colore-au-premier-plan_1340-25699.jpg', 3),
-                                                                 ('Bobs coding journey', 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png', 3);
+INSERT INTO Post (description, image, uploader_id)
+VALUES ('First post by John',
+        'https://img.freepik.com/photos-gratuite/peinture-lac-montagne-montagne-arriere-plan_188544-9126.jpg?size=626&ext=jpg&ga=GA1.1.1788068356.1706832000&semt=sph',
+        1),
+       ('Janes travel adventure',
+        'https://images.ctfassets.net/hrltx12pl8hq/28ECAQiPJZ78hxatLTa7Ts/2f695d869736ae3b0de3e56ceaca3958/free-nature-images.jpg?fit=fill&w=1200&h=630',
+        2),
+       ('Bobs coding journey',
+        'https://img.freepik.com/photos-gratuite/peinture-numerique-montagne-arbre-colore-au-premier-plan_1340-25699.jpg',
+        3),
+       ('Bobs coding journey',
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png',
+        3);
 
 -- Inserting Comments
-INSERT INTO Comment (author_id, comment, post_id) VALUES
-                                                      (2, 'Great post, John!', 1),
-                                                      (3, 'I wish I could travel like this!', 2),
-                                                      (1, 'Awesome coding journey, Bob!', 3);
+INSERT INTO Comment (author_id, comment, post_id)
+VALUES (2, 'Great post, John!', 1),
+       (3, 'I wish I could travel like this!', 2),
+       (1, 'Awesome coding journey, Bob!', 3);
 
 -- Creating Follower-Followee Relationships
-INSERT INTO Person_followers (followers_id, followees_id) VALUES
-                                                              (1, 2), -- John follows Jane
-                                                              (1, 3), -- John follows Bob
-                                                              (2, 1); -- Jane follows John
+INSERT INTO Person_followers (followers_id, followees_id)
+VALUES (1, 2), -- John follows Jane
+       (1, 3), -- John follows Bob
+       (2, 1); -- Jane follows John
