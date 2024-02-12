a10d798866ce1e9dd500889cd8c400f5d6f506ca
Zakariae Moutaouakil <moutaouakilzakariae.cpgea@gmail.com>
2024-02-12 21:03:03 +0100

    Added test mapping

--- src/main/java/com/instagram/demo/controller/AuthController.java
+++ src/main/java/com/instagram/demo/controller/AuthController.java
@@ -5,31 +5,24 @@ import com.instagram.demo.data.repository.PersonRepository;
 import com.instagram.demo.data.schema.Person;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+
 record RegisterUserCredentials(String username, String email, String password, String firstname, String lastname) {
 }
 
 @RestController
 @RequestMapping(path = "/", produces = "application/json")
 @AllArgsConstructor
-@CrossOrigin(
-        origins = "http://localhost:4200",
-        allowCredentials = "true",
-        maxAge = 3600,
-        allowedHeaders = "*",
-        methods= {RequestMethod.GET,RequestMethod.POST,
-                RequestMethod.DELETE, RequestMethod.PUT,
-                RequestMethod.PATCH, RequestMethod.OPTIONS,
-                RequestMethod.HEAD, RequestMethod.TRACE}
-)
+@CrossOrigin(origins = "http://localhost:4200")
 public class AuthController {
     private final PersonRepository personRepository;
     private final PasswordEncoder passwordEncoder;
 
-    @PostMapping("register")
+    @PostMapping(path = "register")
     @ResponseStatus(code = HttpStatus.CREATED, reason = "Your account was successfully created.")
     Person register(@RequestBody RegisterUserCredentials registerUserCredentials) {
         return personRepository.save(new Person(
@@ -40,6 +33,7 @@ public class AuthController {
                 registerUserCredentials.lastname()
         ));
     }
+
     @GetMapping("login")
     public PersonProjection login(Authentication authentication) {
         return personRepository
@@ -47,5 +41,10 @@ public class AuthController {
                 .orElseThrow(() ->
                         new UsernameNotFoundException("User with specified credentials is not found.")
                 );
+    }
+
+    @GetMapping("test")
+    public ResponseEntity<Object> test() {
+        return ResponseEntity.ok().build();
     }
 }
