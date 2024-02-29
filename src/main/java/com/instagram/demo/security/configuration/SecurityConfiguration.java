f62b2638d953d70cfcbf16fe5a16f7428107d852
Zakariae Moutaouakil <moutaouakilzakariae.cpgea@gmail.com>
2024-02-29 10:29:38 +0100

    Replacing CSRF cookie with Strict SameSite policy

--- src/main/java/com/instagram/demo/security/configuration/SecurityConfiguration.java
+++ src/main/java/com/instagram/demo/security/configuration/SecurityConfiguration.java
@@ -1,22 +1,18 @@
 package com.instagram.demo.security.configuration;
 
-import com.instagram.demo.security.filter.CsrfCookieFilter;
 import com.instagram.demo.security.filter.JWTTokenGeneratorFilter;
 import com.instagram.demo.security.filter.JWTTokenValidatorFilter;
 import org.springframework.boot.web.servlet.server.CookieSameSiteSupplier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
@@ -28,24 +24,9 @@ public class SecurityConfiguration {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        CsrfTokenRequestAttributeHandler csrfTokenRequestAttributeHandler = new CsrfTokenRequestAttributeHandler();
-        csrfTokenRequestAttributeHandler.setCsrfRequestAttributeName("_csrf");
-
-        CookieCsrfTokenRepository cookieCsrfTokenRepository = new CookieCsrfTokenRepository();
-        cookieCsrfTokenRepository.setCookieCustomizer(cookie -> {
-            cookie.secure(true);
-            cookie.httpOnly(false);
-        });
-
         return httpSecurity
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .csrf((csrf) -> csrf
-                        .ignoringRequestMatchers(
-                                new AntPathRequestMatcher("/persons", HttpMethod.POST.name())
-                        )
-                        .csrfTokenRequestHandler(csrfTokenRequestAttributeHandler)
-                        .csrfTokenRepository(cookieCsrfTokenRepository)
-                )
+                .csrf(AbstractHttpConfigurer::disable)
                 .cors(c -> {
                     CorsConfigurationSource source = request -> {
                         CorsConfiguration config = new CorsConfiguration();
@@ -59,7 +40,6 @@ public class SecurityConfiguration {
                     };
                     c.configurationSource(source);
                 })
-                .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
                 .addFilterAfter(new JWTTokenGeneratorFilter(), BasicAuthenticationFilter.class)
                 .addFilterBefore(new JWTTokenValidatorFilter(), BasicAuthenticationFilter.class)
                 .authorizeHttpRequests(
@@ -81,6 +61,6 @@ public class SecurityConfiguration {
 
     @Bean
     public CookieSameSiteSupplier applicationCookieSameSiteSupplier() {
-        return CookieSameSiteSupplier.ofNone();
+        return CookieSameSiteSupplier.ofStrict();
     }
 }
